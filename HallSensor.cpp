4b91dfd464f7092a491705d6e79dd0a8666d2a99
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-04-28 13:54:12 -0700

    Update HallSensor.cpp

--- HallSensor.cpp
+++ HallSensor.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+/*#include <Arduino.h>
 #include "HallSensor.h" 
 #include <math.h>
 
@@ -53,4 +53,51 @@ void HallSensor::defineRPMs() {
     }
     
     return definedRPMs
+}*/
+
+#include <Arduino.h>
+#include "HallSensor.h" 
+#include <math.h>
+
+#define OK 0
+#define ERROR 1
+
+HallSensor::HallSensor(int INPUT_PIN) {
+    inputPin = INPUT_PIN;
+    currentTime = millis();
+
+    pinMode(inputPin, INPUT_PULLUP);
+}
+
+int HallSensor::update() {
+    float delta;
+
+    if prevTime != 0 {
+        delta = (float)current_time - (float)prev_time;
+        prevPRM = 60.0 / (delta / 1000.0);
+    }
+    prevTime = currentTime;
+
+    current_time = millis();
+    delta = (float)current_time - (float)prev_time;
+
+    currentPRM = 60.0 / (delta / 1000.0);
+
+    return OK;
+}
+
+float HallSensor::getRPMs() {
+    return currentRPM, prevRPM;
+}
+
+int HallSensor::getPin() {
+    return input_pin;
+}
+
+int HallSensor::setInterpolateState( int interpolateArgument) {
+    interpolateState = interpolateArgument
+}
+
+int HallSensor::getInterpolateState() {
+    return interpolateState
 }
